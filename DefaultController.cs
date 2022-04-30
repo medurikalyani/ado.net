c652bce54b116e9de883f8c977ec8df0f9e826b3
medurikalyani <59552931+medurikalyani@users.noreply.github.com>
2022-04-30 05:54:45 +0000

    Add files via upload

---
+++ DefaultController.cs
@@ -0,0 +1,21 @@
+﻿using databasef.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace databasef.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            FootballEntity f = new FootballEntity();
+            List<Football> ball=f.Footballs.ToList();
+            return View(ball);
+
+        }
+    }
+}
